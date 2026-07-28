c97fa5880ac433da2114a5508f24ae373e11dc81
SzamilWolochowicz <kochan.hubert7@gmail.com>
2026-07-28 23:22:03 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Events, REST, Routes, SlashCommandBuilder } = require("discord.js");
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+connectionString: process.env.URL_Bazy,
+ssl: {
+rejectUnauthorized: false
+}
+});
 
 const app = express();
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
-});
-
-client.once("ready", () => {
-    console.log(`Zalogowano jako ${client.user.tag}`);
 });
 
 client.login(process.env.Token_Discord);
@@ -21,7 +26,73 @@ app.listen(process.env.PORT || 3000, () => {
     console.log("Serwer uruchomiony");
 });
 
-client.once("ready", () => {
-console.log(`Zalogowano jako ${client.user.tag}`);
-console.log(`Serwery: ${client.guilds.cache.size}`);
+const commands = [
+    new SlashCommandBuilder()
+        .setName("ustaw-kanal")
+        .setDescription("Ustaw kanał powiadomień")
+        .addChannelOption(option =>
+            option
+                .setName("kanal")
+                .setDescription("Kanał do wysyłania powiadomień")
+                .setRequired(true)
+        )
+        .toJSON()
+];
+
+client.once("ready", async () => {
+
+    console.log(`Zalogowano jako ${client.user.tag}`);
+    console.log(`Serwery: ${client.guilds.cache.size}`);
+
+    const rest = new REST({ version: "10" })
+        .setToken(process.env.Token_Discord);
+
+    try {
+
+        await rest.put(
+            Routes.applicationCommands(client.user.id),
+            { body: commands }
+        );
+
+        console.log("Komendy zarejestrowane");
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    if (interaction.commandName === "ustaw-kanal") {
+        const kanal = interaction.options.getChannel("kanal");
+        try {
+            await pool.query(`
+                INSERT INTO "Serwery"
+                (id_serwera, id_kanalu)
+                VALUES ($1, $2)
+                ON CONFLICT (id_serwera)
+                DO UPDATE SET
+                id_kanalu = EXCLUDED.id_kanalu
+            `, [
+                interaction.guild.id,
+                kanal.id
+            ]);
+
+            await interaction.reply({
+                content: `Powiadomienia o streamach będą przychodzić na kanał ${kanal}`,
+                ephemeral: true
+            });
+
+        } catch (error) {
+
+            console.error(error);
+
+            await interaction.reply({
+                content: "Błąd zapisu do bazy",
+                ephemeral: true
+            });
+        }
+    }
 });
